ee283bc23263714e58418c6ae8dc1aac54d6440e
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-06-12 16:12:32 -0500

    added function and goat array

--- js/app.js
+++ js/app.js
@@ -10,3 +10,20 @@ function Goats(name,imgSrc) {
      this.voteCount = 0;
      this.viewCount = 0;
 }
+
+let cruisinGoat = new Goats("cruisinGoat", "./img/cruisin-goat.jpeg");
+let floatGoat = new Goats("floatGoat", "./img/float-your-goat.jpeg");
+let kissingGoat = new Goats("kissingGoat", ".img/kissing-goat.jpeg");
+let sassyGoat = new Goats("sassyGoat", "./img/sassy-goat.jpeg");
+let smilingGoat = new Goats("smilingGoat","./img/smiling-goat.jpeg");
+let sweaterGoat = new Goats("sweaterGoat", "./img/sweater-goat.jpeg");
+
+let goatsArray = [];
+
+goatsArray.push(cruisinGoat);
+goatsArray.push(floatGoat);
+goatsArray.push(kissingGoat);
+goatsArray.push(sassyGoat);
+goatsArray.push(smilingGoat);
+goatsArray.push(sweaterGoat);
+
