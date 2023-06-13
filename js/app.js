fe9b1a5c9f23d75aa6883b19dc384db7d5fe705a
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-06-13 17:05:40 -0500

    added functions and goat arrays to js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-'use strict' 
+"use strict";
 
 let votes = document.getElementById("votes");
 let results = document.getElementById("results");
 
+let goat1 = document.getElementById("goat1");
 
-function Goats(name,imgSrc) {
-     this.name = name;
-     this.imgSrc = imgSrc;
-     this.voteCount = 0;
-     this.viewCount = 0;
+function Goats(name, imgSrc) {
+  this.name = name;
+  this.imgSrc = imgSrc;
+  this.voteCount = 0;
+  this.viewCount = 0;
 }
-
 
 let cruisinGoat = new Goats("cruisinGoat", "./img/cruisin-goat.jpg");
 let floatGoat = new Goats("floatGoat", "./img/float-your-goat.jpg");
-let kissingGoat = new Goats("kissingGoat", ".img/kissing-goat.jpg");
+let kissingGoat = new Goats("kissingGoat", "./img/kissing-goat.jpg");
 let sassyGoat = new Goats("sassyGoat", "./img/sassy-goat.jpg");
-let smilingGoat = new Goats("smilingGoat","./img/smiling-goat.jpg");
+let smilingGoat = new Goats("smilingGoat", "./img/smiling-goat.jpg");
 let sweaterGoat = new Goats("sweaterGoat", "./img/sweater-goat.jpg");
 
 let goatsArray = [];
@@ -27,14 +27,20 @@ goatsArray.push(kissingGoat);
 goatsArray.push(sassyGoat);
 goatsArray.push(smilingGoat);
 goatsArray.push(sweaterGoat);
-console.log(goatsArray);
 
-let randomGoats = goatsArray[3];
+goat1.src = goatsArray[0].imgSrc;
+goat2.src = goatsArray[1].imgSrc;
+goat3.src = goatsArray[2].imgSrc;
+goat4.src = goatsArray[3].imgSrc;
+goat5.src = goatsArray[4].imgSrc;
+goat6.src = goatsArray[5].imgSrc;
 
-function randomGoats(goatsArray) {
-     return goatsArray[Math.floor(Math.random() * goatsArray.length)];
-   };
+function getRandomGoats(name, imgSrc) {
+  return goatsArray[Math.floor(Math.random() * goatsArray.length)];
+  
+};
 
-function addImg(){
-     
-}
+let randomGoats = getRandomGoats(goatsArray);
+console.log(randomGoats);
+
+console.log(`Your random goat is `)
