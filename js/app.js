33720c92ed8fbec448932b1b9d821ca6c8f8ccb7
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-06-14 13:05:23 -0500

    added product pictures

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 let votes = document.getElementById("votes");
 let results = document.getElementById("results");
 
-let goat1Img = document.getElementById("prod1");
+let prod1Img = document.getElementById("prod1");
 let prod2Img = document.getElementById("prod2");
 let prod3Img = document.getElementById("prod3");
 let prod4Img = document.getElementById("prod4");
@@ -17,25 +17,25 @@ function Products(name, imgSrc) {
   this.viewCount = 0;
 }
 
-let cruisinprod = new Goats("cruisinGoat", "./img/cruisin-goat.jpg");
-let floatGoat = new Goats("floatGoat", "./img/float-your-goat.jpg");
-let kissingGoat = new Goats("kissingGoat", "./img/kissing-goat.jpg");
-let sassyGoat = new Goats("sassyGoat", "./img/sassy-goat.jpg");
-let smilingGoat = new Goats("smilingGoat", "./img/smiling-goat.jpg");
-let sweaterGoat = new Goats("sweaterGoat", "./img/sweater-goat.jpg");
+let bag = new Products("bag", "./img/bag.jpg");
+let banana = new Products("banana", "./img/banana.jpg");
+let dog = new Products("dog", "./img/dog-duck.jpg");
+let dragon = new Products("dragon", "./img/dragon.jpg");
+let breakfast = new Products("breakfast", "./img/breakfast.jpg");
+let wineGlass = new Products("wineGlass", "./img/wine-glass.jpg");
 
 let prodsArray = [];
 
-prodsArray.push(cruisinprod);
-prodsArray.push(floatprod);
-prodsArray.push(kissingprod);
-prodsArray.push(sassyprod);
-prodsArray.push(smilingprod);
-prodsArray.push(sweaterprod);
+prodsArray.push(bag);
+prodsArray.push(banana);
+prodsArray.push(dog);
+prodsArray.push(dragon);
+prodsArray.push(breakfast);
+prodsArray.push(wineGlass);
 
-let prods2Array = []
+// let prods2Array = []
 
-function setprodsImages(prod1, prod2, prod3, prod4, prod5, prod6) {
+function setProdsImages(prod1, prod2, prod3, prod4, prod5, prod6) {
   prod1Img.src = prod1.imgSrc;
   prod1Img.alt = prod1.name;
   prod2Img.src = prod2.imgSrc;
@@ -49,40 +49,41 @@ function setprodsImages(prod1, prod2, prod3, prod4, prod5, prod6) {
   prod6Img.src = prod6.imgSrc;
   prod6Img.alt = prod6.name;
 }
-setprodsImages(
+
+setProdsImages(
   prodsArray[0],
   prodsArray[1],
   prodsArray[2],
   prodsArray[3],
   prodsArray[4],
-  prodsArray[5]
+  prodsArray[5],
 );
 
 //Step 2 define event handler
-function handleprodsClick(event) {
-  console.log("click");
-  event.preventDefault();
-  let target = event.target;
-  let prodName = target.alt;
+// function handleprodsClick(event) {
+//   console.log("click");
+//   event.preventDefault();
+//   let target = event.target;
+//   let prodName = target.alt;
   
-  for (let i = 0; i < prodsArray.length; i++) {
-    let prods = prodsArray[i];
-    if(prods.name === prodName) {
-      favprod = prods;
-    }
-  }
- console.log(prodName);
-}
+//   for (let i = 0; i < prodsArray.length; i++) {
+//     let prods = prodsArray[i];
+//     if(prods.name === prodName) {
+//       favprod = prods;
+//     }
+//   }
+//  console.log(prodName);
+// }
 
-// //Step 3 add event handler
-votes.addEventListener("click", handleprodsClick);
-
-let favprod;
+// // //Step 3 add event handler
+// votes.addEventListener("click", handleprodsClick);
 
 
-function getRandomprods(name, imgSrc) {
-  return prodsArray[Math.floor(Math.random() * prodsArray.length)];
-}
+
+
+// function getRandomprods(name, imgSrc) {
+//   return prodsArray[Math.floor(Math.random() * prodsArray.length)];
+// }
 
 
 
